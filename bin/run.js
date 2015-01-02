6fafb44198f7f9fb6f6a029308566e1459856a4c
an.han <an.han@qunar.com>
2015-01-03 06:35:59 +0800

    conf

--- bin/run.js
+++ bin/run.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var path =require('path');
 
+var config = path.join(__dirname, '../src/config.json');
+if (!fs.existsSync(config)) {
+    fs.writeFileSync(config, JSON.stringify({}));
+}
 require('./../src/cli')();
